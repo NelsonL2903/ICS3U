c5a42448ba8de529970f0d5ea8e4b72790e377e7
loopnels162 <loopnels162@2060I-L207-23.tvnet.Pri>
2020-03-04 14:40:26 -0500

    more bigx

--- ICS3ULoopNelson/src/main/java/loopexercises/BigX.java
+++ ICS3ULoopNelson/src/main/java/loopexercises/BigX.java
@@ -4,30 +4,7 @@ public class BigX {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 7; ++i) {
-			// spaces for left X
-			for (int j = 0; j < i; ++j) {
-				System.out.print(" ");
-			}
-
-			// print out the left X
-			System.out.print("X");
-
-			// spaces between left X and right X
-			// spaces between two X characters has to decrease
-			// each line
-			for (int j = 0; j < 13 - 2 * i; ++j) {
-				System.out.print(" ");
-			}
-
-			// print out the right X
-			System.out.println("X");
-
-			// central x
-			for (int j = 0; j < 7; ++j) {
-				System.out.print(" ");
-			}
-			System.out.println("X");
+	
 
 		}
 
