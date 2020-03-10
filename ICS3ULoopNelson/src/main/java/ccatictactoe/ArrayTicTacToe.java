dfd35c2da3199ef85a425d999e919dbde65e4d93
JEN <JEN@10.0.0.185>
2020-03-09 20:58:54 -0400

    both versions are done!!

--- ICS3ULoopNelson/src/main/java/ccatictactoe/ArrayTicTacToe.java
+++ ICS3ULoopNelson/src/main/java/ccatictactoe/ArrayTicTacToe.java
@@ -2,22 +2,48 @@ package ccatictactoe;
 
 import java.util.Scanner;
 
-public class ArrayTesting {
+public class ArrayTicTacToe {
 
-	Scanner keyboard = new Scanner(System.in);
+	static Scanner keyboard = new Scanner(System.in);
 	int[][] board = new int[3][3];
 
 	public static void main(String[] args) {
 
-		ArrayTesting gameboard = new ArrayTesting();
-		gameboard.printboard();
-		gameboard.playerturn();
+		ArrayTicTacToe gameboard = new ArrayTicTacToe();
+		String answer;
 
-		gameboard.cft();
+	do {
+			gameboard.cb();
+			
+			gameboard.printboard();
+			gameboard.pt();
 
-		gameboard.printboard();
-		gameboard.playerturn();
+			gameboard.cft();
 
+			do {
+				gameboard.printboard();
+				gameboard.pt();
+
+				if (gameboard.pwc() == false) {
+					if (gameboard.owc() == false) {
+						if (gameboard.swc() == false) {
+							if (gameboard.rc() == false) {
+								gameboard.fc();
+							}
+						}
+					}
+				}
+			} while (gameboard.wc() == false && gameboard.dc() == false);
+
+			gameboard.printboard();
+
+			System.out.println("Would you like to play again?");
+			keyboard.nextLine();
+			answer = keyboard.nextLine();
+			
+		} while (answer.equals("Yes") || answer.equals("yes") || answer.equals("YES") || answer.equals("y")
+				|| answer.equals("Y"));
+		
 	}
 
 	public void printboard() {
@@ -30,12 +56,12 @@ public class ArrayTesting {
 		System.out.println(" ");
 	}
 
-	public void playerturn() {
+	public void pt() {
 		boolean cc = false;
 		do {
-			System.out.println("Please the column number, 1, 2, or 3");
+			System.out.println("Where would you like to play? Please enter the column number, 1, 2, or 3");
 			int column = keyboard.nextInt() - 1;
-			System.out.println("Where would you like to play? Please insert the row number, 1, 2 or 3");
+			System.out.println("Please enter the row number, 1, 2 or 3");
 			int row = keyboard.nextInt() - 1;
 			if (board[row][column] == 0) {
 				board[row][column] = 1;
@@ -361,5 +387,87 @@ public class ArrayTesting {
 		}
 		return false;
 	}
+	
+	public boolean dc() {
+		if (board[0][0] != 0 && board [0][1] != 0 && board [0][2] != 0 && board [1][0] != 0 && 
+				board [1][1] != 0 && board [1][2] != 0 && board [2][0] != 0 && board [2][1] != 0
+				&& board [2][2] != 0) {
+			System.out.println("It's a draw! Good game!");
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean rc() {
+		if (board [1][0] == 1 && board [1][1] == 2 && board [1][2] == 1 && board [0][0] == 0) {
+			board [0][0] = 1;
+			return true;
+		} else if (board [0][1] == 1 && board [1][1] == 2 && board [2][1] == 1 && board [0][2] == 0) {
+			board [0][2] = 1;
+			return true;
+		} else if (board [0][0] == 2 && board [0][1] == 0 && board [0][2] == 1 && board [1][0] == 1 && board [1][1] == 1
+				&& board [1][2] == 2 && board [2][0] == 2 && board [2][1] == 0 && board [2][2] == 1) {
+			board [0][1] = 1;
+			return true;
+		} else if (board [0][0] == 2 && board [0][1] == 1 && board [0][2] == 2 && board [1][0] == 0 && board [1][1] == 1
+				&& board [1][2] == 0 && board [2][0] == 1 && board [2][1] == 2 && board [2][2] == 1) {
+			board [1][2] = 1;
+			return true;
+		} else if (board [0][0] == 1 && board [0][1] == 2 && board [0][2] == 1 && board [1][0] == 0 && board [1][1] == 1
+				&& board [1][2] == 0 && board [2][0] == 2 && board [2][1] == 1 && board [2][2] == 2) {
+			board [1][0] = 1;
+			return true;
+		} else if (board [0][0] == 1 && board [0][1] == 0 && board [0][2] == 2 && board [1][0] == 2 && board [1][1] == 1
+				&& board [1][2] == 1 && board [2][0] == 1 && board [2][1] == 0 && board [2][2] == 2) {
+			board [2][1] = 1;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean fc() {
+		if (board [0][0] == 0) {
+			board [0][0] = 2;
+			return true;
+		} else if (board [0][1] == 0) {
+			board [0][1] = 2;
+			return true;
+		} else if (board [0][2] == 0) {
+			board [0][2] = 2;
+			return true;
+		} else if (board [1][0] == 0) {
+			board [1][0] = 2;
+			return true;
+		} else if (board [1][1] == 0) {
+			board [1][1] = 2;
+			return true;
+		} else if (board [1][2] == 0) {
+			board [1][2] = 2;
+			return true;
+		} else if (board [2][0] == 0) {
+			board [2][0] = 2;
+			return true;
+		} else if (board [2][1] == 0) {
+			board [2][1] = 2;
+			return true;
+		} else if (board [2][2] == 0) {
+			board [2][2] = 2;
+			return true;
+		}
+		return false;
+	}
+	
+	public void cb() {
+		board [0][0] = 0;
+		board [0][1] = 0;
+		board [0][2] = 0;
+		board [1][0] = 0;
+		board [1][1] = 0;
+		board [1][2] = 0;
+		board [2][0] = 0;
+		board [2][1] = 0;
+		board [2][2] = 0;
+	}
+	
 
 }
--- ICS3ULoopNelson/src/main/java/ccatictactoe/TicTacToe.java
+++ ICS3ULoopNelson/src/main/java/ccatictactoe/TicTacToe.java
@@ -4,94 +4,507 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
+	String TL = " ";
+	String TM = " ";
+	String TR = " ";
+	String ML = " ";
+	String MM = " ";
+	String MR = " ";
+	String BL = " ";
+	String BM = " ";
+	String BR = " ";
+
+	static Scanner keyboard = new Scanner(System.in);
+
 	public static void main(String[] args) {
+		TicTacToe nttt = new TicTacToe();
+		String answer;
 
-		Scanner keyboard = new Scanner(System.in);
+		do {
+			nttt.cb();
+			nttt.pt();
 
-		System.out.println("Tic Tac Toe!");
-		System.out.println("   |   |   ");
-		System.out.println("-----------");
-		System.out.println("   |   |   ");
-		System.out.println("-----------");
-		System.out.println("   |   |   ");
-		System.out.println("Would you like to play first or second? Please enter as a number");
-		int order = keyboard.nextInt();
-		keyboard.nextLine();
-		if (order == 2) {
-			System.out.println("   |   | X ");
-			System.out.println("-----------");
-			System.out.println("   |   |   ");
-			System.out.println("-----------");
-			System.out.println("   |   |   ");
-			System.out.println("Where would you like to play? For example: Top Left = TL");
-			String first = keyboard.nextLine();
-			if (first.equals("ML")) {
-				System.out.println("   |   | X ");
-				System.out.println("-----------");
-				System.out.println(" O | X |   ");
-				System.out.println("-----------");
-				System.out.println("   |   |   ");
-				System.out.println("Where would you like to play? For example: Top Left = TL");
-				String second = keyboard.nextLine();
-				if (second.equals("BL")) {
-					System.out.println(" X |   | X ");
-					System.out.println("-----------");
-					System.out.println(" O | X |   ");
-					System.out.println("-----------");
-					System.out.println(" O |   |   ");
-					System.out.println("Where would you like to play? For example: Top Left = TL");
-					String third = keyboard.nextLine();
-					if (third.equals("TM")) {
-						System.out.println(" X | O | X ");
-						System.out.println("-----------");
-						System.out.println(" O | X |   ");
-						System.out.println("-----------");
-						System.out.println(" O |   | X ");
-						System.out.println("Yay! I have won!");
-					} else {
-						System.out.println(" X | X | X ");
-						System.out.println("-----------");
-						System.out.println(" O | X |   ");
-						System.out.println("-----------");
-						System.out.println(" O |   | O ");
-						System.out.println("Yay! I have won!");
-					}
+			nttt.cft();
 
-				}
-			} else if (first.equals("TM")) {
-				System.out.println("   | O | X ");
-				System.out.println("-----------");
-				System.out.println("   | X |   ");
-				System.out.println("-----------");
-				System.out.println("   |   |   ");
-				System.out.println("Where would you like to play? For example: Top Left = TL");
-				String second = keyboard.nextLine();
-				if (second.equals("BL")) {
-					System.out.println("   | O | X ");
-					System.out.println("-----------");
-					System.out.println("   | X |   ");
-					System.out.println("-----------");
-					System.out.println(" O |   | X ");
-					System.out.println("Where would you like to play? For example: Top Left = TL");
-					String third = keyboard.nextLine();
-					if (third.equals("TL")) {
-						System.out.println(" O | O | X ");
-						System.out.println("-----------");
-						System.out.println("   | X | X ");
-						System.out.println("-----------");
-						System.out.println(" O |   | X ");
-						System.out.println("Yay! I have won!");
-					} else {
-						System.out.println(" X | O | X ");
-						System.out.println("-----------");
-						System.out.println("   | X | O ");
-						System.out.println("-----------");
-						System.out.println(" O |   | X ");
-						System.out.println("Yay! I have won!");
+			do {
+				nttt.pt();
+
+				if (nttt.pwc() == false) {
+					if (nttt.owc() == false) {
+						if (nttt.swc() == false) {
+							if (nttt.rc() == false) {
+								nttt.fc();
+							}
+						}
 					}
 				}
+			} while (nttt.wc() == false && nttt.dc() == false);
 
-			}
-		}
+			nttt.pb();
+
+			System.out.println("Would you like to play again?");
+			answer = keyboard.nextLine();
+		} while (answer.equals("Yes") || answer.equals("yes") || answer.equals("YES") || answer.equals("y")
+				|| answer.equals("Y"));
+
 	}
+
+	public void pt() {
+
+		System.out.println("    ");
+		System.out.print(TL + " | ");
+		System.out.print(TM + " | ");
+		System.out.println(TR);
+		System.out.println("---------");
+		System.out.print(ML + " | ");
+		System.out.print(MM + " | ");
+		System.out.println(MR);
+		System.out.println("---------");
+		System.out.print(BL + " | ");
+		System.out.print(BM + " | ");
+		System.out.println(BR);
+		boolean uc = false;
+		System.out.println("Where would you like to play? For example: Top Left = TL");
+		do {
+			System.out.println("Please choose a blank square, or make sure your format is correct");
+			String place1 = keyboard.nextLine();
+			if (place1.equals("TL") && TL.equals(" ")) {
+				TL = "X";
+				uc = true;
+			} else if (place1.equals("TM") && TM.equals(" ")) {
+				TM = "X";
+				uc = true;
+			} else if (place1.equals("TR") && TR.equals(" ")) {
+				TR = "X";
+				uc = true;
+			} else if (place1.equals("ML") && ML.equals(" ")) {
+				ML = "X";
+				uc = true;
+			} else if (place1.equals("MM") && MM.equals(" ")) {
+				MM = "X";
+				uc = true;
+			} else if (place1.equals("MR") && MR.equals(" ")) {
+				MR = "X";
+				uc = true;
+			} else if (place1.equals("BL") && BL.equals(" ")) {
+				BL = "X";
+				uc = true;
+			} else if (place1.equals("BM") && BM.equals(" ")) {
+				BM = "X";
+				uc = true;
+			} else if (place1.equals("BR") && BR.equals(" ")) {
+				BR = "X";
+				uc = true;
+			}
+		} while (uc == false);
+
+	}
+
+	public void cft() {
+		if (TL.equals("X") || TR.equals("X") || BL.equals("X") || BR.equals("X")) {
+			MM = "O";
+		} else if (TM.equals("X") || ML.equals("X") || MR.equals("X") || BM.equals("X")) {
+			MM = "O";
+		} else if (MM.equals("X")) {
+			BL = "O";
+		}
+
+	}
+
+	public boolean pwc() {
+		if (TL.equals("O") && ML.equals("O") && BL.equals(" ")) {
+			BL = "O";
+			return true;
+		} else if (TL.equals(" ") && ML.equals("O") && BL.equals("O")) {
+			TL = "O";
+			return true;
+		} else if (TL.equals("O") && ML.equals(" ") && BL.equals("O")) {
+			ML = "O";
+			return true;
+
+		} else if (TM.equals("O") && MM.equals("O") && BM.equals(" ")) {
+			BM = "O";
+			return true;
+		} else if (TM.equals(" ") && MM.equals("O") && BM.equals("O")) {
+			TM = "O";
+			return true;
+		} else if (TM.equals("O") && MM.equals(" ") && BM.equals("O")) {
+			MM = "O";
+			return true;
+
+		} else if (TR.equals("O") && MR.equals("O") && BR.equals(" ")) {
+			BR = "O";
+			return true;
+		} else if (TR.equals(" ") && MR.equals("O") && BR.equals("O")) {
+			TR = "O";
+			return true;
+		} else if (TR.equals("O") && MR.equals(" ") && BR.equals("O")) {
+			MR = "O";
+			return true;
+
+		} else if (TL.equals("O") && TM.equals("O") && TR.equals(" ")) {
+			TR = "O";
+			return true;
+		} else if (TL.equals(" ") && TM.equals("O") && TR.equals("O")) {
+			TL = "O";
+			return true;
+		} else if (TL.equals("O") && TM.equals(" ") && TR.equals("O")) {
+			TM = "O";
+			return true;
+
+		} else if (ML.equals("O") && MM.equals("O") && MR.equals(" ")) {
+			MR = "O";
+			return true;
+		} else if (ML.equals(" ") && MM.equals("O") && MR.equals("O")) {
+			ML = "O";
+			return true;
+		} else if (ML.equals("O") && MM.equals(" ") && MR.equals("O")) {
+			MM = "O";
+			return true;
+
+		} else if (BL.equals("O") && BM.equals("O") && BR.equals(" ")) {
+			BR = "O";
+			return true;
+		} else if (BL.equals(" ") && BM.equals("O") && BR.equals("O")) {
+			BL = "O";
+			return true;
+		} else if (BL.equals("O") && BM.equals(" ") && BR.equals("O")) {
+			BM = "O";
+			return true;
+
+		} else if (TL.equals("O") && MM.equals("O") && BR.equals(" ")) {
+			BR = "O";
+			return true;
+		} else if (TL.equals(" ") && MM.equals("O") && BR.equals("O")) {
+			TL = "O";
+			return true;
+		} else if (TL.equals("O") && MM.equals(" ") && BR.equals("O")) {
+			MM = "O";
+			return true;
+
+		} else if (BL.equals("O") && MM.equals("O") && TR.equals(" ")) {
+			TR = "O";
+			return true;
+		} else if (BL.equals(" ") && MM.equals("O") && TR.equals("O")) {
+			BL = "O";
+			return true;
+		} else if (BL.equals("O") && MM.equals(" ") && TR.equals("O")) {
+			MM = "O";
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean owc() {
+		if (TL.equals("X") && ML.equals("X") && BL.equals(" ")) {
+			BL = "O";
+			return true;
+		} else if (TL.equals(" ") && ML.equals("X") && BL.equals("X")) {
+			TL = "O";
+			return true;
+		} else if (TL.equals("X") && ML.equals(" ") && BL.equals("X")) {
+			ML = "O";
+			return true;
+
+		} else if (TM.equals("X") && MM.equals("X") && BM.equals(" ")) {
+			BM = "O";
+			return true;
+		} else if (TM.equals(" ") && MM.equals("X") && BM.equals("X")) {
+			TM = "O";
+			return true;
+		} else if (TM.equals("X") && MM.equals(" ") && BM.equals("X")) {
+			MM = "O";
+			return true;
+
+		} else if (TR.equals("X") && MR.equals("X") && BR.equals(" ")) {
+			BR = "O";
+			return true;
+		} else if (TR.equals(" ") && MR.equals("X") && BR.equals("X")) {
+			TR = "O";
+			return true;
+		} else if (TR.equals("X") && MR.equals(" ") && BR.equals("X")) {
+			MR = "O";
+			return true;
+
+		} else if (TL.equals("X") && TM.equals("X") && TR.equals(" ")) {
+			TR = "O";
+			return true;
+		} else if (TL.equals(" ") && TM.equals("X") && TR.equals("X")) {
+			TL = "O";
+			return true;
+		} else if (TL.equals("X") && TM.equals(" ") && TR.equals("X")) {
+			TM = "O";
+			return true;
+
+		} else if (ML.equals("X") && MM.equals("X") && MR.equals(" ")) {
+			MR = "O";
+			return true;
+		} else if (ML.equals(" ") && MM.equals("X") && MR.equals("X")) {
+			ML = "O";
+			return true;
+		} else if (ML.equals("X") && MM.equals(" ") && MR.equals("X")) {
+			MM = "O";
+			return true;
+
+		} else if (BL.equals("X") && BM.equals("X") && BR.equals(" ")) {
+			BR = "O";
+			return true;
+		} else if (BL.equals(" ") && BM.equals("X") && BR.equals("X")) {
+			BL = "O";
+			return true;
+		} else if (BL.equals("X") && BM.equals(" ") && BR.equals("X")) {
+			BM = "O";
+			return true;
+
+		} else if (TL.equals("X") && MM.equals("X") && BR.equals(" ")) {
+			BR = "O";
+			return true;
+		} else if (TL.equals(" ") && MM.equals("X") && BR.equals("X")) {
+			TL = "O";
+			return true;
+		} else if (TL.equals("X") && MM.equals(" ") && BR.equals("X")) {
+			MM = "O";
+			return true;
+
+		} else if (BL.equals("X") && MM.equals("X") && TR.equals(" ")) {
+			TR = "O";
+			return true;
+		} else if (BL.equals(" ") && MM.equals("X") && TR.equals("X")) {
+			BL = "O";
+			return true;
+		} else if (BL.equals("X") && MM.equals(" ") && TR.equals("X")) {
+			MM = "O";
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean swc() {
+		if (TM.equals("X") && BL.equals("X") && TL.equals(" ") && TR.equals(" ") && ML.equals(" ")) {
+			TL = "O";
+			return true;
+
+		} else if (TL.equals("X") && MR.equals("X") && TR.equals(" ") && TM.equals(" ") && BR.equals(" ")) {
+			TR = "O";
+			return true;
+
+		} else if (TR.equals("X") && BM.equals("X") && BR.equals(" ") && MR.equals(" ") && BL.equals(" ")) {
+			BR = "O";
+			return true;
+
+		} else if (ML.equals("X") && BR.equals("X") && BL.equals(" ") && TL.equals(" ") && BM.equals(" ")) {
+			BL = "O";
+			return true;
+
+		} else if (BL.equals("X") && TR.equals("X") && MR.equals(" ") && BM.equals(" ") && BR.equals(" ")) {
+			MR = "O";
+			return true;
+
+		} else if (TL.equals("X") && ML.equals(" ") && BR.equals("X") && BM.equals(" ") && BL.equals(" ")) {
+			ML = "O";
+			return true;
+
+		} else if (BL.equals("X") && MR.equals("X") && BR.equals(" ") && BM.equals(" ") && TR.equals(" ")) {
+			BR = "O";
+			return true;
+
+		} else if (TL.equals("X") && BM.equals("X") && BL.equals(" ") && ML.equals(" ") && BR.equals(" ")) {
+			BL = "O";
+			return true;
+
+		} else if (ML.equals("X") && TR.equals("X") && TL.equals(" ") && TM.equals(" ") && BL.equals(" ")) {
+			TL = "O";
+			return true;
+
+		} else if (TM.equals("X") && BR.equals("X") && TR.equals(" ") && TL.equals(" ") && MR.equals(" ")) {
+			TR = "O";
+			return true;
+
+		} else if (ML.equals("X") && TM.equals("X") && TL.equals(" ") && BL.equals(" ") && TR.equals(" ")) {
+			TL = "O";
+			return true;
+
+		} else if (TM.equals("X") && MR.equals("X") && TR.equals(" ") && TL.equals(" ") && BR.equals(" ")) {
+			TR = "O";
+			return true;
+
+		} else if (BM.equals("X") && MR.equals("X") && BR.equals(" ") && BL.equals(" ") && TR.equals(" ")) {
+			BR = "O";
+			return true;
+
+		} else if (ML.equals("X") && BM.equals("X") && BL.equals(" ") && TL.equals(" ") && BR.equals(" ")) {
+			BL = "O";
+			return true;
+
+		} else if (BL.equals("X") && TR.equals("X") && ML.equals(" ") && TL.equals(" ") && TM.equals(" ")) {
+			ML = "O";
+			return true;
+
+		} else if (TL.equals("X") && BR.equals("X") && MR.equals(" ") && TM.equals(" ") && TR.equals(" ")) {
+			MR = "O";
+			return true;
+
+		} else if (TL.equals(" ") && MM.equals("X") && TR.equals("X") && TM.equals(" ") && ML.equals(" ")) {
+			TL = "O";
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean wc() {
+		if (TL.equals("X") && TM.equals("X") && TR.equals("X")) {
+			System.out.println("You have won!");
+			return true;
+		} else if (ML.equals("X") && MM.equals("X") && MR.equals("X")) {
+			System.out.println("You have won!");
+			return true;
+		} else if (BL.equals("X") && BM.equals("X") && BR.equals("X")) {
+			System.out.println("You have won!");
+			return true;
+
+		} else if (TL.equals("X") && ML.equals("X") && BL.equals("X")) {
+			System.out.println("You have won!");
+			return true;
+		} else if (TM.equals("X") && MM.equals("X") && BM.equals("X")) {
+			System.out.println("You have won!");
+			return true;
+		} else if (TR.equals("X") && MR.equals("X") && BR.equals("X")) {
+			System.out.println("You have won!");
+			return true;
+
+		} else if (TL.equals("X") && MM.equals("X") && BR.equals("X")) {
+			System.out.println("You have won!");
+			return true;
+		} else if (BL.equals("X") && MM.equals("X") && TR.equals("X")) {
+			System.out.println("You have won!");
+			return true;
+
+		} else if (TL.equals("O") && TM.equals("O") && TR.equals("O")) {
+			System.out.println("Computer has won!");
+			return true;
+		} else if (ML.equals("O") && MM.equals("O") && MR.equals("O")) {
+			System.out.println("Computer has won!");
+			return true;
+		} else if (BL.equals("O") && BM.equals("O") && BR.equals("O")) {
+			System.out.println("Computer has won!");
+			return true;
+
+		} else if (TL.equals("O") && ML.equals("O") && BL.equals("O")) {
+			System.out.println("Computer has won!");
+			return true;
+		} else if (TM.equals("O") && MM.equals("O") && BM.equals("O")) {
+			System.out.println("Computer has won!");
+			return true;
+		} else if (TR.equals("O") && MR.equals("O") && BR.equals("O")) {
+			System.out.println("Computer has won!");
+			return true;
+
+		} else if (TL.equals("O") && MM.equals("O") && BR.equals("O")) {
+			System.out.println("Computer has won!");
+			return true;
+		} else if (BL.equals("O") && MM.equals("O") && TR.equals("O")) {
+			System.out.println("Computer has won!");
+			return true;
+		}
+		return false;
+	}
+
+	public boolean dc() {
+		if (!TL.equals(" ") && !TM.equals(" ") && !TR.equals(" ") && !ML.equals(" ") && !MM.equals(" ")
+				&& !MR.equals(" ") && !BL.equals(" ") && !BM.equals(" ") && !BR.equals(" ")) {
+			System.out.println("It's a draw! Good game!");
+			return true;
+		}
+		return false;
+	}
+
+	public boolean rc() {
+		if (ML.equals("X") && MM.equals("O") && MR.equals("X") && TL.equals(" ")) {
+			TL = "O";
+			return true;
+		} else if (TM.equals("X") && MM.equals("O") && BM.equals("X") && TR.equals(" ")) {
+			TR = "O";
+			return true;
+		} else if (TL.equals("O") && TM.equals(" ") && TR.equals("X") && ML.equals("X") && MM.equals("X")
+				&& MR.equals("O") && BL.equals("O") && BM.equals(" ") && BR.equals("X")) {
+			TM = "O";
+			return true;
+		} else if (TL.equals("O") && TM.equals("X") && TR.equals("O") && ML.equals(" ") && MM.equals("X")
+				&& MR.equals(" ") && BL.equals("X") && BM.equals("O") && BR.equals("X")) {
+			MR = "O";
+			return true;
+		} else if (TL.equals("X") && TM.equals("O") && TR.equals("X") && ML.equals(" ") && MM.equals("X")
+				&& MR.equals(" ") && BL.equals("O") && BM.equals("X") && BR.equals("O")) {
+			ML = "O";
+			return true;
+		} else if (TL.equals("X") && TM.equals(" ") && TR.equals("O") && ML.equals("O") && MM.equals("X")
+				&& MR.equals("X") && BL.equals("X") && BM.equals(" ") && BR.equals("O")) {
+			BM = "O";
+			return true;
+		}
+
+		return false;
+	}
+
+	public void pb() {
+		System.out.print(TL + " | ");
+		System.out.print(TM + " | ");
+		System.out.println(TR);
+		System.out.println("---------");
+		System.out.print(ML + " | ");
+		System.out.print(MM + " | ");
+		System.out.println(MR);
+		System.out.println("---------");
+		System.out.print(BL + " | ");
+		System.out.print(BM + " | ");
+		System.out.println(BR);
+	}
+
+	public void cb() {
+		TL = " ";
+		TM = " ";
+		TR = " ";
+		ML = " ";
+		MM = " ";
+		MR = " ";
+		BL = " ";
+		BM = " ";
+		BR = " ";
+	}
+
+	public boolean fc() {
+		if (TL.equals(" ")) {
+			TL = "O";
+			return true;
+		} else if (TM.equals(" ")) {
+			TM = "O";
+			return true;
+		} else if (TR.equals(" ")) {
+			TR = "O";
+			return true;
+		} else if (ML.equals(" ")) {
+			ML = "O";
+			return true;
+		} else if (MM.equals(" ")) {
+			MM = "O";
+			return true;
+		} else if (MR.equals(" ")) {
+			MR = "O";
+			return true;
+		} else if (BL.equals(" ")) {
+			BL = "O";
+			return true;
+		} else if (BM.equals(" ")) {
+			BM = "O";
+			return true;
+		} else if (BR.equals(" ")) {
+			BR = "O";
+			return true;
+		}
+		return false;
+	}
+
 }
