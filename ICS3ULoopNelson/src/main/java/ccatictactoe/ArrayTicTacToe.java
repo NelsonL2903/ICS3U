721d7e9d377f448020fc98826b5ce12d0d718a2c
loopnels162 <loopnels162@2060I-L207-23.tvnet.Pri>
2020-03-10 14:41:01 -0400

    debugging!

--- ICS3ULoopNelson/src/main/java/ccatictactoe/ArrayTicTacToe.java
+++ ICS3ULoopNelson/src/main/java/ccatictactoe/ArrayTicTacToe.java
@@ -12,9 +12,9 @@ public class ArrayTicTacToe {
 		ArrayTicTacToe gameboard = new ArrayTicTacToe();
 		String answer;
 
-	do {
+		do {
 			gameboard.cb();
-			
+
 			gameboard.printboard();
 			gameboard.pt();
 
@@ -40,10 +40,10 @@ public class ArrayTicTacToe {
 			System.out.println("Would you like to play again?");
 			keyboard.nextLine();
 			answer = keyboard.nextLine();
-			
+
 		} while (answer.equals("Yes") || answer.equals("yes") || answer.equals("YES") || answer.equals("y")
 				|| answer.equals("Y"));
-		
+
 	}
 
 	public void printboard() {
@@ -63,9 +63,14 @@ public class ArrayTicTacToe {
 			int column = keyboard.nextInt() - 1;
 			System.out.println("Please enter the row number, 1, 2 or 3");
 			int row = keyboard.nextInt() - 1;
-			if (board[row][column] == 0) {
-				board[row][column] = 1;
-				cc = true;
+			if (row < 3 && row > -1 && column < 3 && column > -1) {
+				if (board[row][column] == 0) {
+					board[row][column] = 1;
+					cc = true;
+				} else {
+					System.out.println("Please input another coordinate, and make sure it is blank");
+					cc = false;
+				}
 			} else {
 				System.out.println("Please input another coordinate, and make sure it is blank");
 				cc = false;
@@ -387,87 +392,85 @@ public class ArrayTicTacToe {
 		}
 		return false;
 	}
-	
+
 	public boolean dc() {
-		if (board[0][0] != 0 && board [0][1] != 0 && board [0][2] != 0 && board [1][0] != 0 && 
-				board [1][1] != 0 && board [1][2] != 0 && board [2][0] != 0 && board [2][1] != 0
-				&& board [2][2] != 0) {
+		if (board[0][0] != 0 && board[0][1] != 0 && board[0][2] != 0 && board[1][0] != 0 && board[1][1] != 0
+				&& board[1][2] != 0 && board[2][0] != 0 && board[2][1] != 0 && board[2][2] != 0) {
 			System.out.println("It's a draw! Good game!");
 			return true;
 		}
 		return false;
 	}
-	
+
 	public boolean rc() {
-		if (board [1][0] == 1 && board [1][1] == 2 && board [1][2] == 1 && board [0][0] == 0) {
-			board [0][0] = 1;
+		if (board[1][0] == 1 && board[1][1] == 2 && board[1][2] == 1 && board[0][0] == 0) {
+			board[0][0] = 1;
 			return true;
-		} else if (board [0][1] == 1 && board [1][1] == 2 && board [2][1] == 1 && board [0][2] == 0) {
-			board [0][2] = 1;
+		} else if (board[0][1] == 1 && board[1][1] == 2 && board[2][1] == 1 && board[0][2] == 0) {
+			board[0][2] = 1;
 			return true;
-		} else if (board [0][0] == 2 && board [0][1] == 0 && board [0][2] == 1 && board [1][0] == 1 && board [1][1] == 1
-				&& board [1][2] == 2 && board [2][0] == 2 && board [2][1] == 0 && board [2][2] == 1) {
-			board [0][1] = 1;
+		} else if (board[0][0] == 2 && board[0][1] == 0 && board[0][2] == 1 && board[1][0] == 1 && board[1][1] == 1
+				&& board[1][2] == 2 && board[2][0] == 2 && board[2][1] == 0 && board[2][2] == 1) {
+			board[0][1] = 1;
 			return true;
-		} else if (board [0][0] == 2 && board [0][1] == 1 && board [0][2] == 2 && board [1][0] == 0 && board [1][1] == 1
-				&& board [1][2] == 0 && board [2][0] == 1 && board [2][1] == 2 && board [2][2] == 1) {
-			board [1][2] = 1;
+		} else if (board[0][0] == 2 && board[0][1] == 1 && board[0][2] == 2 && board[1][0] == 0 && board[1][1] == 1
+				&& board[1][2] == 0 && board[2][0] == 1 && board[2][1] == 2 && board[2][2] == 1) {
+			board[1][2] = 1;
 			return true;
-		} else if (board [0][0] == 1 && board [0][1] == 2 && board [0][2] == 1 && board [1][0] == 0 && board [1][1] == 1
-				&& board [1][2] == 0 && board [2][0] == 2 && board [2][1] == 1 && board [2][2] == 2) {
-			board [1][0] = 1;
+		} else if (board[0][0] == 1 && board[0][1] == 2 && board[0][2] == 1 && board[1][0] == 0 && board[1][1] == 1
+				&& board[1][2] == 0 && board[2][0] == 2 && board[2][1] == 1 && board[2][2] == 2) {
+			board[1][0] = 1;
 			return true;
-		} else if (board [0][0] == 1 && board [0][1] == 0 && board [0][2] == 2 && board [1][0] == 2 && board [1][1] == 1
-				&& board [1][2] == 1 && board [2][0] == 1 && board [2][1] == 0 && board [2][2] == 2) {
-			board [2][1] = 1;
+		} else if (board[0][0] == 1 && board[0][1] == 0 && board[0][2] == 2 && board[1][0] == 2 && board[1][1] == 1
+				&& board[1][2] == 1 && board[2][0] == 1 && board[2][1] == 0 && board[2][2] == 2) {
+			board[2][1] = 1;
 			return true;
 		}
 		return false;
 	}
-	
+
 	public boolean fc() {
-		if (board [0][0] == 0) {
-			board [0][0] = 2;
+		if (board[0][0] == 0) {
+			board[0][0] = 2;
 			return true;
-		} else if (board [0][1] == 0) {
-			board [0][1] = 2;
+		} else if (board[0][1] == 0) {
+			board[0][1] = 2;
 			return true;
-		} else if (board [0][2] == 0) {
-			board [0][2] = 2;
+		} else if (board[0][2] == 0) {
+			board[0][2] = 2;
 			return true;
-		} else if (board [1][0] == 0) {
-			board [1][0] = 2;
+		} else if (board[1][0] == 0) {
+			board[1][0] = 2;
 			return true;
-		} else if (board [1][1] == 0) {
-			board [1][1] = 2;
+		} else if (board[1][1] == 0) {
+			board[1][1] = 2;
 			return true;
-		} else if (board [1][2] == 0) {
-			board [1][2] = 2;
+		} else if (board[1][2] == 0) {
+			board[1][2] = 2;
 			return true;
-		} else if (board [2][0] == 0) {
-			board [2][0] = 2;
+		} else if (board[2][0] == 0) {
+			board[2][0] = 2;
 			return true;
-		} else if (board [2][1] == 0) {
-			board [2][1] = 2;
+		} else if (board[2][1] == 0) {
+			board[2][1] = 2;
 			return true;
-		} else if (board [2][2] == 0) {
-			board [2][2] = 2;
+		} else if (board[2][2] == 0) {
+			board[2][2] = 2;
 			return true;
 		}
 		return false;
 	}
-	
+
 	public void cb() {
-		board [0][0] = 0;
-		board [0][1] = 0;
-		board [0][2] = 0;
-		board [1][0] = 0;
-		board [1][1] = 0;
-		board [1][2] = 0;
-		board [2][0] = 0;
-		board [2][1] = 0;
-		board [2][2] = 0;
+		board[0][0] = 0;
+		board[0][1] = 0;
+		board[0][2] = 0;
+		board[1][0] = 0;
+		board[1][1] = 0;
+		board[1][2] = 0;
+		board[2][0] = 0;
+		board[2][1] = 0;
+		board[2][2] = 0;
 	}
-	
 
 }
