a96131f392e813b659532ae0c72aade5254abf5e
JEN <JEN@10.0.0.185>
2020-04-12 18:24:31 -0400

    hii

--- ICS3ULoopNelson/src/main/java/commandlineargs/CommandLineArgsExample.java
+++ ICS3ULoopNelson/src/main/java/commandlineargs/CommandLineArgsExample.java
@@ -3,7 +3,10 @@ package commandlineargs;
 public class CommandLineArgsExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		for (int i = 0; i < args.length; ++i) {
+			System.out.println("args[" + i + "] is " + args[i]);
+		}
 
 	}
 
