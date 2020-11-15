88074a21c866eadd24c8b429e9107e0178dffb9d
JEN <JEN@10.0.0.185>
2020-11-15 17:15:30 -0500

    imdb works

--- ICS3ULoopNelson/src/main/java/stuffforfun/ForFunStuff.java
+++ ICS3ULoopNelson/src/main/java/stuffforfun/ForFunStuff.java
@@ -10,13 +10,20 @@ public class ForFunStuff {
 
 	public static void main(String[] args) throws IOException {
 
-		Document doc = Jsoup.connect("https://www.imdb.com/title/tt1270797/?ref_=fn_al_tt_1").get();
+		Document doc = Jsoup.connect("https://www.imdb.com/title/tt2278388/?ref_=nv_sr_srsg_3").get();
 
 		String title = doc.title();
 		System.out.println("title: " + title);
 
 		Elements review = doc.select("div.ratingValue strong");
-		System.out.println(review);
+
+		String strtitle = review.toString();
+		String imdbreview = strtitle.substring(15, 18);
+		System.out.println(strtitle);
+		System.out.println(imdbreview);
+		double doubleimdb = Double.valueOf(imdbreview) * 10;
+		int finalimdb = (int) doubleimdb;
+		System.out.println("IMDB Review: " + finalimdb + "%");
 
 	}
 
