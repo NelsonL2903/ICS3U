0bdd7911447a5e161e898845815f436d23ece436
JEN <JEN@10.0.0.185>
2020-11-15 19:19:50 -0500

    IMDB Works!

--- ICS3ULoopNelson/src/main/java/stuffforfun/ForFunStuff.java
+++ ICS3ULoopNelson/src/main/java/stuffforfun/ForFunStuff.java
@@ -1,6 +1,7 @@
 package stuffforfun;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,17 +11,32 @@ public class ForFunStuff {
 
 	public static void main(String[] args) throws IOException {
 
-		Document doc = Jsoup.connect("https://www.imdb.com/title/tt2278388/?ref_=nv_sr_srsg_3").get();
+		Scanner keyboard = new Scanner(System.in);
 
-		String title = doc.title();
-		System.out.println("title: " + title);
+		System.out.println("Please input the movie you are searching for");
+		String movie = keyboard.nextLine();
+		String isl = "http://www.google.com/search?q=imdb " + movie + "&num=1";
 
-		Elements review = doc.select("div.ratingValue strong");
+		Document isd = Jsoup.connect(isl).get();
+		String iilt = isd.title();
+		System.out.println(iilt);
+		Elements results = isd.select("div.yuRUbf a");
+		// System.out.println(results);
+		String aimdb = results.toString();
+		String imdblink = aimdb.substring(9, 46);
+		System.out.println(imdblink);
+
+		Document imdbdoc = Jsoup.connect(imdblink).get();
+
+		String title = imdbdoc.title();
+		System.out.println(title);
+
+		Elements review = imdbdoc.select("div.ratingValue strong");
 
 		String strtitle = review.toString();
 		String imdbreview = strtitle.substring(15, 18);
-		System.out.println(strtitle);
-		System.out.println(imdbreview);
+		// System.out.println(strtitle);
+		// System.out.println(imdbreview);
 		double doubleimdb = Double.valueOf(imdbreview) * 10;
 		int finalimdb = (int) doubleimdb;
 		System.out.println("IMDB Review: " + finalimdb + "%");
